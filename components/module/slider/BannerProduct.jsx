1f42521b89b81aca78866d94f9d7453950c8b8e3
Alireza Fatemi <fa1381138100@gmail.com>
2023-11-21 14:05:01 +0330

    Created the Details Page

--- components/module/slider/BannerProduct.jsx
+++ components/module/slider/BannerProduct.jsx
@@ -9,120 +9,139 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 export default function SliderProduct({ images }) {
-    console.log(images)
-    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
-    return (
-        <div className='banner-product'>
-            <Swiper
-                style={{
-                    '--swiper-navigation-color': '#fff',
-                    '--swiper-pagination-color': '#fff',
-                }}
-                spaceBetween={10}
-                navigation={true}
-                thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-                modules={[FreeMode, Navigation, Thumbs]}
-                className="mySwiper2"
-            >
-                {
-                    images.map((image, index) => (
+  return (
+    <div className='banner-product'>
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
+          "--swiper-navigation-size": "25px",
+        }}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
+      >
+        {
+          images.map((image, index) => (
 
-                        <SwiperSlide key={index}>
-                            <img src={image} alt="product photo" />
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
-            <Swiper
-                onSwiper={setThumbsSwiper}
-                spaceBetween={10}
-                slidesPerView={4}
-                freeMode={true}
-                watchSlidesProgress={true}
-                modules={[FreeMode, Navigation, Thumbs]}
-                className="mySwiper"
-            >
-                {
-                    images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img src={image} alt="product photo" />
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        {
+          images.map((image, index) => (
 
-                        <SwiperSlide key={index}>
-                            <img src={image} alt="product photo" />
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+            <SwiperSlide key={index}>
+              <img src={image} alt="product photo" />
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
 
-            <style>{`
+      <style>{`
 
             .banner-product {
-  max-width: 1600px;
-  margin: 0 auto;
-  position: relative;
-  height: 300px;
-  width: 100vw;
-  padding: 1rem;
-}
+                max-width: 1600px;
+                margin: 0 auto;
+                position: relative;
+                height: 300px;
+                width: 100vw;
+                padding: 1rem;
+              }
 
-.banner-product .swiper-slide img {
-  object-fit: cover;
-}
+              .banner-product .swiper-slide img {
+                object-fit: cover;
+              }
 
-.banner-product .swiper {
-  width: 100%;
-  height: 300px;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.banner-product .swiper-slide {
-  background-size: cover;
-  background-position: center;
-}
-
-.banner-product .mySwiper2 {
-  height: 80%;
-  width: 100% ;
-  text-align: center;
-}
-
-.banner-product .mySwiper2 img {
-    width: fit-content;
-    margin: auto;
-    height: 100% ;
-  }
-
-.banner-product .mySwiper {
-  height: 20%;
-  box-sizing: border-box;
-  padding: 10px 0;
-}
-  
-.banner-product .mySwiper img {
-    width: 100%;
-    height: 80px;
-    cursor: pointer;
-  }
-
-.banner-product .mySwiper .swiper-slide {
-  width: 25%;
-  height: 100%;
-  opacity: 0.4;
-}
-
-.banner-product .mySwiper .swiper-slide-thumb-active {
-  opacity: 1;
-}
-
-@media ( min-width: 768px ) {
-    .banner-product {
-      height: 500px;
-      padding: 1rem 2rem;
-    }
+              .banner-product .swiper {
+                width: 100%;
+                height: 300px;
+                margin-left: auto;
+                margin-right: auto;
+                padding: 10px 0;
     
-}
+              }
+
+              .banner-product .swiper-slide {
+                background-size: cover;
+                background-position: center;
+              }
+
+              .banner-product .mySwiper2 {
+                height: 80%;
+                width: 100% ;
+                text-align: center;
+              }
+
+              .banner-product .mySwiper2 img {
+                  width: fit-content;
+                  margin: auto;
+                  height: 100% ;
+                  padding: 1rem;
+                  background-color: #fff;
+                  border-radius: 10px;
+                }
+                
+              .banner-product .mySwiper {
+                height: 20%;
+                box-sizing: border-box;
+                padding: 10px 0;
+                text-align:center;
+              }
+                
+              .banner-product .mySwiper img {
+                  width: 80px;
+                  height: 40px;
+                  cursor: pointer;
+                }
+
+              .banner-product .mySwiper .swiper-slide {
+                width: 25%;
+                height: 100%;
+                opacity: 0.4;
+              }
+
+              .banner-product .mySwiper .swiper-slide-thumb-active {
+                opacity: 1;
+              }
+
+              @media ( min-width: 768px ) {
+                  .banner-product {
+                    height: 500px;
+                    padding: 1rem 2rem;
+                  }
+
+                  .banner-product .mySwiper {
+                    width:60%;
+                  }
+
+                  .banner-product .mySwiper img {
+                  width: 100px;
+                }                  
+              }
+
+              @media ( min-width: 940px ) {
+                .banner-product .mySwiper img {
+                  width: 140px;
+                }                  
+              }
 
         `}</style>
-        </div>
-    );
+    </div>
+  );
 }
--- components/template/details/DetailsPage.jsx
+++ components/template/details/DetailsPage.jsx
@@ -1,12 +1,80 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 //Component
 import SliderProduct from "../../module/slider/BannerProduct";
 
+//Icon
+import Cross from "../../icons/Cross";
+
+//Style
+import styles from "./DetailsPage.module.scss";
+
+//Icon
+import Star from "../../icons/Star";
+
 const DetailsPage = ({ productData }) => {
 
-    const { title, thumbnail: image, price, discountPercentage, rating, id ,brand ,category ,description ,images ,stock } = productData;
+    const { title, price, discountPercentage: dis, rating, brand, category, description, images, stock } = productData;
+    const router = useRouter();
+    const [isCross, setIsCross] = useState(false);
+
+    const crossHandler = () => {
+        setIsCross(true)
+        setTimeout(() => router.back(), 200)
+    }
+
     return (
-        <div>
+        <div className={styles.detailsPage} style={isCross ? { animation: "UnShowPage .3s forwards" } : { animation: "ShowPage .3s forward" }} >
+            <span onClick={crossHandler} ><Cross /></span>
             <SliderProduct images={images} />
+
+            <div className={ styles.fields } >
+                <div className={ styles.field1 } >
+                    <h2> {title} </h2>
+
+                    <span>
+                        {rating}
+                        <Star />
+                    </span>
+                </div>
+
+                {
+                    dis ?
+                        <div className={ styles.field2 } >
+                            <p> {price} $ </p>
+
+                            <div className={ styles.field2Dis } >
+                                <span> {((100 - dis) * (price / 100)).toFixed(2)} $ </span>
+                                <span> {dis} % </span>
+                            </div>
+                        </div> :
+                        <div className={ styles.field2 } >
+                            <p> {price} </p>
+                        </div>
+                }
+
+                <div className={ styles.field3 } >
+                    <span> Brand: </span>
+                    <span> {brand} </span>
+                </div>
+                <div className={ styles.field4 } >
+                    <span> BranCategory: </span>
+                    <Link href={`/category/${category}`} > {category} </Link>
+                </div>
+
+                <div className={ styles.field5 } >
+                    <span>Description:</span>
+                    <p> {description} </p>
+                </div>
+
+                <div className={ styles.field6 } >
+                    <button> Add to Cart </button>
+                </div>
+
+                <div></div>
+            </div>
         </div>
     );
 };
