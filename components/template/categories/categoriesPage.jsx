319290f2b5c5cafb930a97674d9fc4b0274dcd5a
Alireza Fatemi <fa1381138100@gmail.com>
2023-11-22 21:50:39 +0330

    edited

--- components/template/categories/categoriesPage.jsx
+++ components/template/categories/categoriesPage.jsx
@@ -4,8 +4,8 @@ const CategoriesPage = ({ categories }) => {
     return (
         <div>
             {
-                categories.map( item => (
-                    <Link href={`/search?q=${item}`} > { item } </Link>
+                categories.map( (item ,index) => (
+                    <Link href={`/search?q=${item}`} key={index} > { item } </Link>
                 ) )
             }
         </div>
